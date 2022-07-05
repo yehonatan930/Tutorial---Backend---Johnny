c6f290af133d86cc4be750084e3ab5718967a429
Dvir Raviv <devir0raviv@gmail.com>
2022-07-05 15:49:26 +0300

    added connection config

--- src/typeorm/typeorm.module.ts
+++ src/typeorm/typeorm.module.ts
@@ -15,6 +15,17 @@ export const TypeormModule = TypeOrmModule.forRootAsync({
     entities: [TemplateEntity, Audit], // here are written all the DB entities
     synchronize: false, //where true all entities will be updated according to the models.
     schema: configService.get<string>('SERVER_SCHEMA'),
+    extra: {
+      connectionLimit: 50,// how many connection are open to the db concurrently
+      max : 50 //the same...
+    },
+    connectTimeoutMS: 20000, // how much time before the connection times out,
+                      // here it's 20 seconds. no matter what, the connection stops
+                      //after this rime period.
+    retryAttempts: 2, //how many times to retry to connect
+    retryDelay: 2000, // cool down between failed connection to a retry
+    keepConnectionAlive: true, 
+    logging:true, // logs the queries
   }),
   inject: [ConfigService],
 });
