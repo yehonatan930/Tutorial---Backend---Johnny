88a267895e3163a37002b49d41c7a4a3bf89630e
Dvir Raviv <devir0raviv@gmail.com>
2022-08-15 12:52:56 +0300

    fixed health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { Public } from '../guards/authentication/types/authorizationTypes';
 
 
-@Controller('healthCheck')
+@Controller()
 export class HealthController {
   constructor() {}
 
